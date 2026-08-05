8896c0de44f46e7f172bb57f1abe434d981c0ce0
joaolucaspcd <sebastiaodonizetimariano@gmail.com>
2026-08-05 14:39:40 -0300

    05/08

---
+++ atividade1/atividade1_dom/script.js
@@ -0,0 +1,13 @@
+                const texto = document.getElementById('spanTexto') 
+
+
+                    const botaoalterartexto = document.getElementById('btnalterartexto')
+
+
+                    botaoalterartexto.addEventListener('click', () => {
+                        texto.textContent = 'ESSA É MINHA ATIVIDADE';
+                                
+                    });
+         
+
+            
